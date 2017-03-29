19d3d79735c4083f7b3aad1550ee669ea0aafc0f
Bip Thelin <bip@kivra.com>
2017-03-29 11:49:58 +0200

    Fix deploy

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Kivra Documentation
+
+# Running
+```shell
+$ docker compose up
+```
+
+# Deploying
+```shell
+$ ./deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ Options:
                            commit's message.
 "
 
-bundle exec middleman build --clean
+docker run --rm -v $PWD:/usr/src/app/source -w /usr/src/app/source developerkivracom_app bundle exec middleman build --clean
 
 parse_args() {
   # Set args from a local environment file.
