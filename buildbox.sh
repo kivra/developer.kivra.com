4dd486c8b0b3456e0a9d8a3e2f9ad7ccb508c288
Bip Thelin <bip@kivra.com>
2018-04-30 11:31:54 +0200

    Style fixes

--- buildbox.sh
+++ buildbox.sh
@@ -3,7 +3,7 @@ set -o errexit #abort if any command fails
 
 echo '--- building'
 docker build -t developerkivracom_app .
-docker run --rm -v ${PWD}/source:/usr/src/app/source developerkivracom_app widdershins documentation/index.yaml -o source/index.html.md
+docker run --rm -v ${PWD}/source:/usr/src/app/source developerkivracom_app widdershins -u templates documentation/index.yaml -o source/index.html.md
 
 echo '--- deploying'
 GIT_DEPLOY_EMAIL=$BUILDKITE_BUILD_CREATOR_EMAIL GIT_DEPLOY_USERNAME=$BUILDKITE_BUILD_CREATOR ./deploy.sh
