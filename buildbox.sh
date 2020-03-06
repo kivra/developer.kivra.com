2b9756c3b8eff77cf07c2de195a230042ab17a57
Bip Thelin <bip@kivra.com>
2020-03-06 11:40:11 +0100

    feat: Use swagger-ui

--- buildbox.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit #abort if any command fails
-
-echo '--- building'
-docker build -t developerkivracom_app .
-docker run --rm -v ${PWD}/source:/usr/src/app/source developerkivracom_app widdershins -u templates documentation/index.yaml -o source/index.html.md
-
-echo '--- deploying'
-GIT_DEPLOY_EMAIL=$BUILDKITE_BUILD_CREATOR_EMAIL GIT_DEPLOY_USERNAME=$BUILDKITE_BUILD_CREATOR ./deploy.sh
